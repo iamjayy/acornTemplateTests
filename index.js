b3f33dd63d7bebd6e4ca3900242b1780f7626194
DigitalMarketing <iamjayy>
2022-10-03 12:26:26 -0400

    updated file

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
 window.addEventListener("scroll", function () {
   const header = document.querySelector("header");
-  header.classList.toggle("sticky", window.scrollY > 0);
+  header.classList.toggle("sticky", window.scrollY > 3500);
 });
